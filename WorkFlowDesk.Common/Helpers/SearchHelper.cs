a71a0bbccf9086bd8a0bf85806a9005a06bfc0ee
Pablo <pablo.arenas@gracia.lasalle.cat>
2026-01-19 11:25:33 +0100

    Agregar filtros y bÃºsqueda bÃ¡sica

---
+++ WorkFlowDesk.Common/Helpers/SearchHelper.cs
@@ -0,0 +1,34 @@
+using System.Linq.Expressions;
+
+namespace WorkFlowDesk.Common.Helpers;
+
+public static class SearchHelper
+{
+    public static IEnumerable<T> FilterByText<T>(IEnumerable<T> items, string searchText, params Func<T, string>[] propertySelectors)
+    {
+        if (string.IsNullOrWhiteSpace(searchText))
+            return items;
+
+        searchText = searchText.ToLowerInvariant();
+
+        return items.Where(item =>
+            propertySelectors.Any(selector =>
+            {
+                var value = selector(item);
+                return value?.ToLowerInvariant().Contains(searchText) ?? false;
+            })
+        );
+    }
+
+    public static IEnumerable<T> FilterByProperty<T, TProperty>(
+        IEnumerable<T> items,
+        Expression<Func<T, TProperty>> propertySelector,
+        TProperty? value) where TProperty : IEquatable<TProperty>
+    {
+        if (value == null)
+            return items;
+
+        var compiledSelector = propertySelector.Compile();
+        return items.Where(item => compiledSelector(item).Equals(value));
+    }
+}
