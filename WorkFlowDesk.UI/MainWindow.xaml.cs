4fc7b7a3da7a347631d76f045c55db38f7cc67c6
Pablo <pablo.arenas@gracia.lasalle.cat>
2026-01-19 11:23:16 +0100

    Conectar navegaciÃ³n del sidebar

--- WorkFlowDesk.UI/MainWindow.xaml.cs
+++ WorkFlowDesk.UI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using System.Windows;
 using WorkFlowDesk.UI.Services;
+using WorkFlowDesk.UI.Views;
+using WorkFlowDesk.ViewModel.ViewModels;
 
 namespace WorkFlowDesk.UI
 {
@@ -9,12 +11,45 @@ namespace WorkFlowDesk.UI
     public partial class MainWindow : Window
     {
         private readonly NavigationService _navigationService;
+        private readonly MainViewModel _mainViewModel;
 
         public MainWindow()
         {
             InitializeComponent();
             _navigationService = new NavigationService();
             _navigationService.Initialize(ContentArea);
+            
+            _mainViewModel = new MainViewModel();
+            _mainViewModel.NavigateRequested += OnNavigateRequested;
+            DataContext = _mainViewModel;
+            
+            // Navegar a Dashboard por defecto
+            _navigationService.NavigateTo<DashboardView>();
+        }
+
+        private void OnNavigateRequested(object? sender, string viewName)
+        {
+            switch (viewName)
+            {
+                case "Dashboard":
+                    _navigationService.NavigateTo<DashboardView>();
+                    break;
+                case "Empleados":
+                    var empleadosService = ServiceLocator.GetService<Services.Interfaces.IEmpleadoService>();
+                    var empleadosViewModel = new EmpleadosViewModel(empleadosService);
+                    _navigationService.NavigateTo(new EmpleadosView(empleadosViewModel));
+                    break;
+                case "Proyectos":
+                    var proyectosService = ServiceLocator.GetService<Services.Interfaces.IProyectoService>();
+                    var proyectosViewModel = new ProyectosViewModel(proyectosService);
+                    _navigationService.NavigateTo(new ProyectosView(proyectosViewModel));
+                    break;
+                case "Tareas":
+                    var tareasService = ServiceLocator.GetService<Services.Interfaces.ITareaService>();
+                    var tareasViewModel = new TareasViewModel(tareasService);
+                    _navigationService.NavigateTo(new TareasView(tareasViewModel));
+                    break;
+            }
         }
     }
 }
---
+++ WorkFlowDesk.ViewModel/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+using CommunityToolkit.Mvvm.Input;
+using WorkFlowDesk.Services.Interfaces;
+using WorkFlowDesk.ViewModel.Base;
+
+namespace WorkFlowDesk.ViewModel.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public IRelayCommand NavigateToDashboardCommand { get; }
+    public IRelayCommand NavigateToEmpleadosCommand { get; }
+    public IRelayCommand NavigateToProyectosCommand { get; }
+    public IRelayCommand NavigateToTareasCommand { get; }
+    public IRelayCommand NavigateToClientesCommand { get; }
+    public IRelayCommand NavigateToReportesCommand { get; }
+    public IRelayCommand NavigateToConfiguracionCommand { get; }
+
+    public event EventHandler<string>? NavigateRequested;
+
+    public MainViewModel()
+    {
+        NavigateToDashboardCommand = new RelayCommand(() => NavigateRequested?.Invoke(this, "Dashboard"));
+        NavigateToEmpleadosCommand = new RelayCommand(() => NavigateRequested?.Invoke(this, "Empleados"));
+        NavigateToProyectosCommand = new RelayCommand(() => NavigateRequested?.Invoke(this, "Proyectos"));
+        NavigateToTareasCommand = new RelayCommand(() => NavigateRequested?.Invoke(this, "Tareas"));
+        NavigateToClientesCommand = new RelayCommand(() => NavigateRequested?.Invoke(this, "Clientes"));
+        NavigateToReportesCommand = new RelayCommand(() => NavigateRequested?.Invoke(this, "Reportes"));
+        NavigateToConfiguracionCommand = new RelayCommand(() => NavigateRequested?.Invoke(this, "Configuracion"));
+    }
+}
