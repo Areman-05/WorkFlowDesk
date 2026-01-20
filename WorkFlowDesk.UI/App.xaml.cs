17242971734812dba5c05356293058ba2f6e3d0f
Pablo <pablo.arenas@gracia.lasalle.cat>
2026-01-20 11:19:51 +0100

    Mejorar LoginView para conectar con autenticaciÃ³n

--- WorkFlowDesk.UI/App.xaml.cs
+++ WorkFlowDesk.UI/App.xaml.cs
@@ -1,5 +1,9 @@
 using System.Windows;
+using WorkFlowDesk.Common.Services;
+using WorkFlowDesk.Domain.Entities;
 using WorkFlowDesk.UI.Services;
+using WorkFlowDesk.UI.Views;
+using WorkFlowDesk.ViewModel.ViewModels;
 
 namespace WorkFlowDesk.UI
 {
@@ -12,6 +16,24 @@ namespace WorkFlowDesk.UI
         {
             base.OnStartup(e);
             ServiceLocator.ConfigureServices();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var authService = ServiceLocator.GetService<Services.Interfaces.IAuthenticationService>();
+            var loginViewModel = new LoginViewModel(authService);
+            var loginView = new LoginView(loginViewModel);
+
+            loginViewModel.LoginExitoso += (s, usuario) =>
+            {
+                SessionService.SetCurrentUser(usuario);
+                loginView.Close();
+                
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+            };
+
+            loginView.ShowDialog();
         }
     }
 }
