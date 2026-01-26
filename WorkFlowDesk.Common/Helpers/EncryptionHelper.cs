378899ce7553ae49cf36d07f4969f4815746bf2e
Pablo <pablo.arenas@gracia.lasalle.cat>
2026-01-26 12:16:47 +0100

    Crear helpers de encriptaciÃ³n

---
+++ WorkFlowDesk.Common/Helpers/EncryptionHelper.cs
@@ -0,0 +1,81 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WorkFlowDesk.Common.Helpers;
+
+public static class EncryptionHelper
+{
+    public static string Encrypt(string plainText, string key)
+    {
+        if (string.IsNullOrEmpty(plainText))
+            return string.Empty;
+
+        byte[] iv = new byte[16];
+        byte[] array;
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(key.PadRight(32).Substring(0, 32));
+            aes.IV = iv;
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(plainText);
+                    }
+
+                    array = memoryStream.ToArray();
+                }
+            }
+        }
+
+        return Convert.ToBase64String(array);
+    }
+
+    public static string Decrypt(string cipherText, string key)
+    {
+        if (string.IsNullOrEmpty(cipherText))
+            return string.Empty;
+
+        byte[] iv = new byte[16];
+        byte[] buffer = Convert.FromBase64String(cipherText);
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(key.PadRight(32).Substring(0, 32));
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (MemoryStream memoryStream = new MemoryStream(buffer))
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader streamReader = new StreamReader(cryptoStream))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+
+    public static string GenerateRandomKey(int length = 32)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var random = new Random();
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    public static string HashString(string input)
+    {
+        using var sha256 = SHA256.Create();
+        var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+        return Convert.ToBase64String(hashedBytes);
+    }
+}
