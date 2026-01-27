54b167c20571b60fb3316a4b43d0e68cf665fcbb
Pablo <pablo.arenas@gracia.lasalle.cat>
2026-01-27 11:05:33 +0100

    Crear vista TareaFormView

---
+++ WorkFlowDesk.UI/Views/TareaFormView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace WorkFlowDesk.UI.Views;
+
+public partial class TareaFormView : UserControl
+{
+    public TareaFormView()
+    {
+        InitializeComponent();
+    }
+}
