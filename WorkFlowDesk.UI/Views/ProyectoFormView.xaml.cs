bc96a7d9d85438a9f3597fb5234e523d6a99da8d
Pablo <pablo.arenas@gracia.lasalle.cat>
2026-01-27 11:03:39 +0100

    Crear vista ProyectoFormView

---
+++ WorkFlowDesk.UI/Views/ProyectoFormView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace WorkFlowDesk.UI.Views;
+
+public partial class ProyectoFormView : UserControl
+{
+    public ProyectoFormView()
+    {
+        InitializeComponent();
+    }
+}
