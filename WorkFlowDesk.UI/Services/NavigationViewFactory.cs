3bc69d787204879af688f3f340910eb53422bdae
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2026-06-12 11:34:38 +0200

    refactor: NavigationViewFactory usa inyeccion de dependencias

--- WorkFlowDesk.UI/Services/NavigationViewFactory.cs
+++ WorkFlowDesk.UI/Services/NavigationViewFactory.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
 using WorkFlowDesk.Services.Interfaces;
 using WorkFlowDesk.UI.Views;
 using WorkFlowDesk.ViewModel.ViewModels;
@@ -8,71 +9,85 @@ namespace WorkFlowDesk.UI.Services;
 /// <summary>Crea vistas y ViewModels para la navegación principal.</summary>
 public static class NavigationViewFactory
 {
-    public static UserControl CreateView(string viewName)
+    public static UserControl CreateView(string viewName, IServiceProvider? services = null)
     {
+        services ??= ServiceLocator.Provider;
+
         return viewName switch
         {
-            "Dashboard" => CreateDashboard(),
-            "Empleados" => CreateEmpleados(),
-            "Proyectos" => CreateProyectos(),
-            "Tareas" => CreateTareas(),
-            "Clientes" => CreateClientes(),
-            "Reportes" => CreateReportes(),
-            "Configuracion" => CreateConfiguracion(),
+            "Dashboard" => CreateDashboard(services),
+            "Empleados" => CreateEmpleados(services),
+            "Proyectos" => CreateProyectos(services),
+            "Tareas" => CreateTareas(services),
+            "Clientes" => CreateClientes(services),
+            "Reportes" => CreateReportes(services),
+            "Configuracion" => CreateConfiguracion(services),
             _ => throw new ArgumentException($"Vista desconocida: {viewName}", nameof(viewName))
         };
     }
 
-    private static UserControl CreateDashboard()
+    private static UserControl CreateDashboard(IServiceProvider services)
     {
-        var empleadoService = ServiceLocator.GetService<IEmpleadoService>();
-        var proyectoService = ServiceLocator.GetService<IProyectoService>();
-        var tareaService = ServiceLocator.GetService<ITareaService>();
-        var clienteService = ServiceLocator.GetService<IClienteService>();
-        var viewModel = new DashboardViewModel(empleadoService, proyectoService, tareaService, clienteService);
+        var viewModel = new DashboardViewModel(
+            services.GetRequiredService<IEmpleadoService>(),
+            services.GetRequiredService<IProyectoService>(),
+            services.GetRequiredService<ITareaService>(),
+            services.GetRequiredService<IClienteService>());
         return new DashboardView(viewModel);
     }
 
-    private static UserControl CreateEmpleados()
+    private static UserControl CreateEmpleados(IServiceProvider services)
     {
-        var empleadoService = ServiceLocator.GetService<IEmpleadoService>();
-        var exportService = ServiceLocator.GetService<IExportService>();
-        return new EmpleadosView(new EmpleadosViewModel(empleadoService, exportService));
+        var empleadoService = services.GetRequiredService<IEmpleadoService>();
+        var exportService = services.GetRequiredService<IExportService>();
+        return new EmpleadosView(new EmpleadosViewModel(empleadoService, exportService), empleadoService);
     }
 
-    private static UserControl CreateProyectos()
+    private static UserControl CreateProyectos(IServiceProvider services)
     {
-        var proyectoService = ServiceLocator.GetService<IProyectoService>();
-        var exportService = ServiceLocator.GetService<IExportService>();
-        return new ProyectosView(new ProyectosViewModel(proyectoService, exportService));
+        var proyectoService = services.GetRequiredService<IProyectoService>();
+        var clienteService = services.GetRequiredService<IClienteService>();
+        var empleadoService = services.GetRequiredService<IEmpleadoService>();
+        var exportService = services.GetRequiredService<IExportService>();
+        return new ProyectosView(
+            new ProyectosViewModel(proyectoService, exportService),
+            proyectoService,
+            clienteService,
+            empleadoService);
     }
 
-    private static UserControl CreateTareas()
+    private static UserControl CreateTareas(IServiceProvider services)
     {
-        var tareaService = ServiceLocator.GetService<ITareaService>();
-        var exportService = ServiceLocator.GetService<IExportService>();
-        return new TareasView(new TareasViewModel(tareaService, exportService));
+        var tareaService = services.GetRequiredService<ITareaService>();
+        var proyectoService = services.GetRequiredService<IProyectoService>();
+        var empleadoService = services.GetRequiredService<IEmpleadoService>();
+        var exportService = services.GetRequiredService<IExportService>();
+        return new TareasView(
+            new TareasViewModel(tareaService, exportService),
+            tareaService,
+            proyectoService,
+            empleadoService);
     }
 
-    private static UserControl CreateClientes()
+    private static UserControl CreateClientes(IServiceProvider services)
     {
-        var clienteService = ServiceLocator.GetService<IClienteService>();
-        var exportService = ServiceLocator.GetService<IExportService>();
-        return new ClientesView(new ClientesViewModel(clienteService, exportService));
+        var clienteService = services.GetRequiredService<IClienteService>();
+        var exportService = services.GetRequiredService<IExportService>();
+        return new ClientesView(new ClientesViewModel(clienteService, exportService), clienteService);
     }
 
-    private static UserControl CreateReportes()
+    private static UserControl CreateReportes(IServiceProvider services)
     {
-        var reporteService = ServiceLocator.GetService<IReporteService>();
-        var exportService = ServiceLocator.GetService<IExportService>();
+        var reporteService = services.GetRequiredService<IReporteService>();
+        var exportService = services.GetRequiredService<IExportService>();
         return new ReportesView(new ReportesViewModel(reporteService, exportService));
     }
 
-    private static UserControl CreateConfiguracion()
+    private static UserControl CreateConfiguracion(IServiceProvider services)
     {
-        var backupService = ServiceLocator.GetService<IBackupService>();
-        var dbInitService = ServiceLocator.GetService<IDatabaseInitializationService>();
-        var authService = ServiceLocator.GetService<IAuthenticationService>();
-        return new ConfiguracionView(new ConfiguracionViewModel(backupService, dbInitService, authService));
+        return new ConfiguracionView(new ConfiguracionViewModel(
+            services.GetRequiredService<IBackupService>(),
+            services.GetRequiredService<IDatabaseInitializationService>(),
+            services.GetRequiredService<IAuthenticationService>()));
     }
 }
